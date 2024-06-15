6ebaa22c8a5063fd9a51fbca2e0397e0e6dfa5bd
Amber Sultan <ambersultan124@gmail.com>
2024-06-15 21:12:01 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [programName, setProgramName] = useState("ByteWise Fellowship Program");
+
+  const changeProgramName = () => {
+    setProgramName("ByteWise Fellowship Program");
+  };
+
   return (
     <div className="App">
-      <h1>This repository about ByteWise Fellowship Program</h1>
+      <h1>This repository is about {programName}</h1>
+      <button onClick={changeProgramName}>Change Program Name</button>
     </div>
   );
 }
